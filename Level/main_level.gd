061679e218920f243ac64b0540dfce6905a01aa7
Devanush Joseph <102316343+Torchyy@users.noreply.github.com>
2025-09-27 10:57:32 -0600

    Added a Default Font

--- Level/main_level.gd
+++ Level/main_level.gd
@@ -1,1 +1,8 @@
 extends Node2D
+class_name MainLevel
+
+# Array of Arrray containing seat coordinates and availability
+# [Vector2, bool]
+@onready var seat_availability := [
+	[Vector2(0,0), false],
+]
