d2fc8dee64eefe3ef381772dc39d4667279aee5f
izmac-z <marco212k4@gmail.com>
2025-09-27 13:34:26 -0600

    function for checking player direction and matching direction for sprite

--- Player/player.gd
+++ Player/player.gd
@@ -25,7 +25,50 @@ func get_movement_input():
 		velocity = input_vector.normalized() * speed
 	
 	sprite.rotation = input_vector.angle()
+	
+	# Change sprite direction
+	if input_vector != Vector2.ZERO:
+		var direction_faced = get_direction_faced(input_vector)
+		
+		match direction_faced:
+			"right": sprite.rotation = 0
+			"down_right": sprite.rotation = PI/4
+			"down": sprite.rotation = PI/2
+			"down_left": sprite.rotation = 3*PI/4
+			"left": sprite.rotation = PI
+			"up_left": sprite.rotation = -3*PI/4
+			"up": sprite.rotation = -PI/2
+			"up_right": sprite.rotation = -PI/4
+			
+		
 	move_and_slide()
+
+# Use vectors to determine the sprite direction
+func get_direction_faced(vector: Vector2) -> String:
+	var angle = vector.angle()
+
+	if angle > -PI/8 and angle <= PI/8:
+		return "right"
+	elif angle > PI/8 and angle <= 3*PI/8: 
+		return "down_right" 
+	elif angle > 3*PI/8 and angle <= 5*PI/8:
+		return "down"
+	elif angle > 5*PI/8 and angle <= 7*PI/8:
+		return "down_left"
+	elif angle > 7*PI/8 and angle <= -7*PI/8:
+		return "left"
+	elif angle > -7*PI/8 and angle <= -5*PI/8:
+		return "up_left"
+	elif angle > -5*PI/8 and angle <= -3*PI/8:
+		return "up"
+	elif angle > -3*PI/8 and angle <= -PI/8:
+		return "up_right"
+	else:
+		return "down"
+	
+	
+
+
 		
 	
 func _physics_process(delta: float) -> void:
