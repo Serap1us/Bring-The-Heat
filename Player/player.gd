23ec2af2b5fe3ab0052e8c041cadccb7bb26f56e
izmac-z <marco212k4@gmail.com>
2025-09-27 11:44:25 -0600

    Added placeholder code for sprite rotation

--- Player/player.gd
+++ Player/player.gd
@@ -2,9 +2,12 @@ class_name Player
 
 extends CharacterBody2D
 
+# Variables
 @export var speed: int = 250
-@export var player_idx: int = 2 # player 1 = idx 1, player 2 = idx 2
+@export var player_idx: int = 1 # player 1 = idx 1, player 2 = idx 2
+@onready var sprite = $Sprite2D
 
+# function to get player input for movement
 func get_movement_input():
 	var input_vector = Vector2.ZERO
 	
@@ -16,13 +19,17 @@ func get_movement_input():
 		input_vector.y -= 1
 	if Input.is_action_pressed("player" + str(player_idx) + "_down"):
 		input_vector.y += 1
-		
-	velocity = input_vector * speed
+	if Input.is_action_pressed("player" + str(player_idx) + "_rotate"):
+		velocity = Vector2.ZERO
+	else:
+		velocity = input_vector.normalized() * speed
 	
-
+	sprite.rotation = input_vector.angle()
+	move_and_slide()
+		
+	
 func _physics_process(delta: float) -> void:
 	get_movement_input()
 	
-	move_and_slide()
 	 
 	
