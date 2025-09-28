87db75643f9eff212e6c3cc55ed8b6a1225365e0
izmac-z <marco212k4@gmail.com>
2025-09-27 18:20:35 -0600

    creating the pause and quiz menu

--- Level/main_level.gd
+++ Level/main_level.gd
@@ -42,3 +42,23 @@ func _set_score(new_score):
 	).set_ease(Tween.EASE_IN).set_delay(0.20)
 	await tween.finished
 	number.queue_free()
+
+
+# Pause menu
+@onready var pause_menu = $PauseMenu
+var paused = false
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("pause"):
+		pauseMenu()
+		
+func pauseMenu():
+	if paused:
+		pause_menu.hide()
+		Engine.time_scale = 1
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+		
+	paused = !paused
